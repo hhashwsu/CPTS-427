6b90971a93a7f19749e1aa4241f6d54eb68ec0fb
Haven Hash <hhash@wsu.edu>
2022-02-26 02:24:12 -0500

    Spacing and concat pass

--- settings.php
+++ settings.php
@@ -21,28 +21,28 @@ function wookitty_orders_sql_init() {
 	// charsets or engine types, generic SQL is fine for our purposes.
 	// N.B.: This gives us persistance and no errors on reload, but for an upgrade with table changes we will
 	// likely need to employ add_option() with test_db_version to do the ALTER statements.
-	$sql = "CREATE TABLE IF NOT EXISTS $db_wookitty_orders_tbl (wc_order_id INT PRIMARY KEY, wookitty_sync_status VARCHAR(15), " .
-	    "wookitty_sync_time TIMESTAMP, wookitty_sync_details VARCHAR(60), cats_customer_num INT, cats_invoice_num INT);";
+	$sql = 'CREATE TABLE IF NOT EXISTS ' . $db_wookitty_orders_tbl . ' (wc_order_id INT PRIMARY KEY, wookitty_sync_status VARCHAR(15), ' .
+		'wookitty_sync_time TIMESTAMP, wookitty_sync_details VARCHAR(60), cats_customer_num INT, cats_invoice_num INT);';
 	dbDelta( $sql );
 
-	$sql = "CREATE TABLE IF NOT EXISTS $db_wookitty_logs_tbl (log_timestamp TIMESTAMP, wookitty_log_mesg VARCHAR(256),
-	    log_source VARCHAR(32));";
-	dbDelta ( $sql );
+	$sql = 'CREATE TABLE IF NOT EXISTS ' . $db_wookitty_logs_tbl . ' (log_timestamp TIMESTAMP, wookitty_log_mesg VARCHAR(256),' .
+		'log_source VARCHAR(32));';
+	dbDelta( $sql );
 
-	$sql = $wpdb->prepare( "SHOW TABLES LIKE'".$db_wookitty_settings_tbl."' ;");
+	$sql = $wpdb->prepare( 'SHOW TABLES LIKE\'' . $db_wookitty_settings_tbl . "\' ;");
 	$res = $wpdb->get_results( $sql );
 
-	$sql = "CREATE TABLE IF NOT EXISTS $db_wookitty_settings_tbl (single_row INT NOT NULL PRIMARY KEY CHECK (single_row = 1),
-	    cats_autosync BOOLEAN DEFAULT 0, rest_username VARCHAR(256) , system_error BOOLEAN, error_mesg VARCHAR(256));";
-	dbDelta ( $sql );
+	$sql = 'CREATE TABLE IF NOT EXISTS' . $db_wookitty_settings_tbl . '(single_row INT NOT NULL PRIMARY KEY CHECK (single_row = 1),' .
+	    'cats_autosync BOOLEAN DEFAULT 0, rest_username VARCHAR(256) , system_error BOOLEAN, error_mesg VARCHAR(256));';
+	dbDelta( $sql );
 
 	// Populate the one and only settings row
 	if ( 0 == count( $res ) ) {
-		$sql = $wpdb->prepare( "INSERT INTO $db_wookitty_settings_tbl (single_row, cats_autosync, rest_username, ".
-		    "system_error,  error_mesg) VALUES (%d, %d, %s, %d, %s);", 1, 0, "wsupress_restuser", 0, "");
+		$sql = $wpdb->prepare( 'INSERT INTO ' . $db_wookitty_settings_tbl . ' (single_row, cats_autosync, rest_username, ' .
+		    'system_error,  error_mesg) VALUES (%d, %d, %s, %d, %s);', 1, 0, 'wsupress_restuser', 0, '');
 		$res = $wpdb->get_results( $sql );
-		wookitty_log("WooKitty SQL tables created and initialized.");
+		wookitty_log('WooKitty SQL tables created and initialized.');
 	}
 
-	wookitty_log("WooKitty plugin activated.");
+	wookitty_log('WooKitty plugin activated.');
 }
