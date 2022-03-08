1955a9fd6de44618485cae4916ae62d65ea4b686
Haven Hash <hhash@wsu.edu>
2022-03-08 01:38:41 -0500

    Whitespace issue

--- settings.php
+++ settings.php
@@ -11,7 +11,7 @@
  */
 
 // XXX: Passes code compliance checks with php7.3, but not with php7.4
-if ( isset( $_POST['action'] ) ) { 
+if ( isset( $_POST['action'] ) ) {
 	if ( wp_verify_nonce( $_POST['wookitty_config_nonce'], 'wookitty_config' ) ) {
 		echo 'We VERIFIED';
 	} else {
