108156a4979e220adc0ec7af4949556bf3e4549e
Haven Hash <hhash@wsu.edu>
2022-03-08 01:24:04 -0500

    Move comment to create function

--- settings.php
+++ settings.php
@@ -9,8 +9,8 @@
  * plugin and client daemon.
  *
  */
-/*
-function wookitty_test() {
+
+function wookitty_test() {/*
 	if ( isset( $_POST['action'] ) ) {
 		if ( wp_verify_nonce( $_POST['wookitty_config_nonce'], 'wookitty_config' ) ) {
 			echo 'We VERIFIED';
@@ -28,8 +28,8 @@ function wookitty_test() {
 				$msg = 'Label Printing activated.';
 				break;
 		}
-	}
-}*/
+	}*/
+}
 
 function wookitty_orders_sql_init() {
 	global $wpdb;
